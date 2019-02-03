1441f6614b5a009266cff3dba087e25c01fdf22f
SevenScore <jrkoeldev@gmail.com>
2019-02-03 15:54:25 -0600

    Create GameAI.cs

---
+++ Quoridor/Assets/GameCore/GameCore/GameAI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCore
+{
+
+    /*
+     * MonteCarloNode is 
+     */
+    class MonteCarloNode
+    {
+        private static List<MonteCarloNode> children;
+        private static MonteCarloNode parent;
+        private static char[,] boardState;
+        private static double winRate;
+        private static PlayerCoordinate AI, Opponent;
+        private static int timesNodeHasBeenVisited;
+        private static string move;
+
+        public static List<MonteCarloNode> GetChildrenNodes()
+        {
+            return children;
+        }
+
+        public static MonteCarloNode GetParentNode()
+        {
+            return parent;
+        }
+
+    }
+    class MonteCarlo
+    {
+    }
+}
