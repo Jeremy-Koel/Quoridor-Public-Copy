631224bde5fb24770a29a8720d2660c9751ea593
Noah Kinslow <nkinslow@harding.edu>
2019-02-03 14:27:14 -0600

    Add Multiplayer User Panels and Buttons
    Add LoginPanel to MainMenu scene:
        - Back button
        - Username input field
        - Password input field
        - Login button (missing functionality)
        - Registration switch button (switches to registration panel)
    
    Add RegistrationPanel to MainMenu scene:
        - Back button
        - Display name input field
        - Username input field
        - Password input field
        - Register button (missing functionality)

--- Quoridor/Assets/Scripts/MainMenu.cs
+++ Quoridor/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour
     public GameObject playModePanel;
     public GameObject difficultyLevelPanel;
     public GameObject settingsPanel;
+    public GameObject loginPanel;
+    public GameObject registrationPanel;
     //public GameObject previousPanel;
     //public GameObject currentPanel;
     public Stack<GameObject> panelOrder;
@@ -21,6 +23,8 @@ public class MainMenu : MonoBehaviour
         playModePanel = GameObject.Find("PlayModePanel");
         difficultyLevelPanel = GameObject.Find("DifficultyLevelPanel");
         settingsPanel = GameObject.Find("SettingsPanel");
+        loginPanel = GameObject.Find("LoginPanel");
+        registrationPanel = GameObject.Find("RegistrationPanel");
         // previousPanel = new GameObject();
         //currentPanel = new GameObject();
         panelOrder = new Stack<GameObject>();
@@ -29,6 +33,8 @@ public class MainMenu : MonoBehaviour
         playModePanel.SetActive(false);
         difficultyLevelPanel.SetActive(false);
         settingsPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        registrationPanel.SetActive(false);
     }
 
     public void onPlayButtonClick()
@@ -48,6 +54,32 @@ public class MainMenu : MonoBehaviour
 
         difficultyLevelPanel.SetActive(true);
         panelOrder.Push(difficultyLevelPanel);
+    }
+
+    public void onMultiplayerClick()
+    {
+        playModePanel.SetActive(false);
+
+        loginPanel.SetActive(true);
+        panelOrder.Push(loginPanel);
+    }
+
+    public void onLoginClick()
+    {
+        // Try to login using username and password
+    }
+
+    public void onRegistrationSwitchClick()
+    {
+        loginPanel.SetActive(false);
+
+        registrationPanel.SetActive(true);
+        panelOrder.Push(registrationPanel);
+    }
+
+    public void onRegistrationClick()
+    {
+        // Try to register new user account using displayname, username, and password
     }
 
     public void onEasyButtonClick()
