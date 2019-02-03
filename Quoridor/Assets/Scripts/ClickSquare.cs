85dc3ea33921029e7bedcd3a07df6b5272eac379
bhudgins <35714322+bhudgins@users.noreply.github.com>
2019-02-03 16:08:34 -0600

    Can Select MOuse

--- Quoridor/Assets/Scripts/ClickSquare.cs
+++ Quoridor/Assets/Scripts/ClickSquare.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class ClickSquare : MonoBehaviour
 {
     public GameObject mouse;
+    private bool mouseSelected;
+    private ClickMouse test;
 
     // Start is called before the first frame update
     void Start()
     {
         //Debug.Log("ADDED");
         mouse = GameObject.Find("playerMouse");
+
+        test = mouse.GetComponent<ClickMouse>();
+
+        
+        mouseSelected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(test.mouseSelected)
+        {
+            Debug.Log("Clicked Mouse");
+        }
+
     }
 
     private void OnMouseUp()
     {
         Debug.Log(this.name);
         mouse.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -0.5f);
+    }
+
+    public void OnClickToggleMouse()
+    {
+        mouseSelected = !mouseSelected;
     }
 }
