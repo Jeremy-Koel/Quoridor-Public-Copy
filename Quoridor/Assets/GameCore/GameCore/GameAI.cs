d7c87047a3dda574c2a616f97f18833eb242c456
Jeremy-Koel <38506627+Jeremy-Koel@users.noreply.github.com>
2019-02-05 22:14:02 -0600

    Expansion of MCTS basics

--- Quoridor/Assets/GameCore/GameCore/GameAI.cs
+++ Quoridor/Assets/GameCore/GameCore/GameAI.cs
@@ -5,31 +5,79 @@ using System.Text;
 namespace GameCore
 {
 
-    /*
-     * MonteCarloNode is 
-     */
+
+    /// <summary>
+    /// Class Name: MonteCarloNode
+    /// Description: MonteCarloNode is a node to be used in the building of a Monte Carlo Search Tree. The constructor for a Monte Carlo node
+    /// accepts a Gameboard.
+    /// </summary>
     class MonteCarloNode
     {
-        private static List<MonteCarloNode> children;
-        private static MonteCarloNode parent;
-        private static char[,] boardState;
-        private static double winRate;
-        private static PlayerCoordinate AI, Opponent;
-        private static int timesNodeHasBeenVisited;
-        private static string move;
+        private List<MonteCarloNode> children;
+        private MonteCarloNode parent;
+        private double winRate;
+        private int timesVisited;
+        public GameBoard boardState;
+        private string thisMove;
 
-        public static List<MonteCarloNode> GetChildrenNodes()
+        public MonteCarloNode(GameBoard newState)
+        {
+            boardState = new GameBoard(newState);
+            children = new List<MonteCarloNode>();
+            parent = null;
+        }
+
+        public MonteCarloNode(MonteCarloNode childParent, GameBoard newState, string move)
+        {
+            parent = childParent;
+            boardState = newState;
+            thisMove = move;
+            if (!move.Contains("v") && !move.Contains("h"))
+            {
+                boardState.MovePiece(boardState.GetWhoseTurn() == 1 ? GameBoard.PlayerEnum.ONE : GameBoard.PlayerEnum.TWO, new PlayerCoordinate(move));
+            }
+            boardState.PrintBoard();
+        }
+
+        public List<MonteCarloNode> GetChildrenNodes()
         {
             return children;
         }
 
-        public static MonteCarloNode GetParentNode()
+        public MonteCarloNode GetParentNode()
         {
             return parent;
+        }
+
+        /// <summary>
+        /// <c>InsertChild</c> method adds a new MonteCarloNode to the MonteCarlo Tree by taking in a move determined as a string
+        /// and converting it into its proper GameBoard state.If the move provided places a wall, a new instance of GameBoard is
+        /// created. Otherwise a normal pawn move will result in the current node's GameBoard being used.
+        /// </summary>
+        /// <param name="move">specified move - either place a wall or move a pawn</param>
+        public void InsertChild(string move)
+        {
+            if (move.Contains("v") || move.Contains("h"))
+            {
+                GameBoard newState = new GameBoard(boardState);
+                newState.PlaceWall(newState.GetWhoseTurn() == 1 ? GameBoard.PlayerEnum.ONE : GameBoard.PlayerEnum.TWO, new WallCoordinate(move));
+                children.Add(new MonteCarloNode(this, newState, move));
+            }
+            else
+            {
+                 children.Add(new MonteCarloNode(this, boardState, move));
+            }
         }
 
     }
     class MonteCarlo
     {
+        static void Main()
+        {
+            MonteCarloNode insertTest = new MonteCarloNode(new GameBoard(GameBoard.PlayerEnum.ONE, "e1", "e9"));
+            insertTest.InsertChild("e2");
+            insertTest.InsertChild("e4h");
+            insertTest.InsertChild("e6v");
+        }
     }
 }
