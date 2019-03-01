727578b4150a0380a9da29c8557930f89ccba093
cspn <cspann1@harding.edu>
2019-03-01 12:10:48 -0600

    Add saving to sound options in settings

---
+++ Quoridor/Assets/AudioControllerMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControllerMainMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(PlayerPrefs.GetFloat("MasterVolume", 1.0f)) * 50);
+        mixer.SetFloat("BackgroundVolume", Mathf.Log10(PlayerPrefs.GetFloat("BackgroundVolume", 1.0f)) * 50);
+        mixer.SetFloat("EffectsVolume", Mathf.Log10(PlayerPrefs.GetFloat("EffectsVolume", 1.0f)) * 50);
+    }
+    
+}
---
+++ Quoridor/Assets/Scripts/AudioSettingController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettingController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Slider masterVolumeSlider = GameObject.Find("MasterVolumeSlider").GetComponent<Slider>();
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1.0f);
+
+        Slider backgroundVolumeSlider = GameObject.Find("BackgroundVolumeSlider").GetComponent<Slider>();
+        backgroundVolumeSlider.value = PlayerPrefs.GetFloat("BackgroundVolume", 1.0f);
+
+        Slider effectsVolumeSlider = GameObject.Find("EffectsVolumeSlider").GetComponent<Slider>();
+        effectsVolumeSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 1.0f);
+    }
+
+    public void SetMasterLevel(float sliderValue)
+    {
+        mixer.SetFloat("MasterVolume", GetMixerValueFromSliderValue(sliderValue));
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+    }
+
+    public void SetBackgroundLevel(float sliderValue)
+    {
+        mixer.SetFloat("BackgroundVolume", GetMixerValueFromSliderValue(sliderValue));
+        PlayerPrefs.SetFloat("BackgroundVolume", sliderValue);
+    }
+
+    public void SetEffectsLevel(float sliderValue)
+    {
+        mixer.SetFloat("EffectsVolume", GetMixerValueFromSliderValue(sliderValue));
+        PlayerPrefs.SetFloat("EffectsVolume", sliderValue);
+    }
+
+    private float GetMixerValueFromSliderValue(float f)
+    {
+        return Mathf.Log10(f) * 50;
+    }
+}
