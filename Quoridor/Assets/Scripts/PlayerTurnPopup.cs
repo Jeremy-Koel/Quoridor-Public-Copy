3a5e5ac35a67dafeb76c8232a75d9f73ce9f7ec4
Noah Kinslow <nedthenetworker155@gmail.com>
2019-03-02 09:57:29 -0600

    Clean up some code

--- Quoridor/Assets/Scripts/PlayerTurnPopup.cs
+++ Quoridor/Assets/Scripts/PlayerTurnPopup.cs
@@ -10,17 +10,12 @@ public class PlayerTurnPopup : MonoBehaviour
     public Controller controller; 
 
     private float popupTime = 2f;
-    //how long should we turn for.
 
     public bool isPoppedUp = false;
-    //are we currently popped up?
-    //we use this to prevent multiple instance of popping up starting.
 
     private GameObject playerTurnPopup;
     private GameObject playerTurnPopupText;
     private Text turnText;
-
-    //private bool initialInactive;
 
     private string PLAYERNAME = "Player";
     private string COMPUTERNAME = "Computer";
@@ -72,44 +67,32 @@ public class PlayerTurnPopup : MonoBehaviour
 
     void Update()
     {
-        //if (initialInactive)
-        //{
-        //    playerTurnPopup.SetActive(false);
-        //    initialInactive = false;
-        //}
-        //if (isPoppedUp)
-        //    StartCoroutine(PopUp());
+
     }
 
     public void UpdateTurnPopup()
     {
-        //if (!isPoppedUp)
+        isPoppedUp = true;
+        playerTurnPopup.SetActive(true);
+            
+            
+        // Get info from ChallengeManager/Controller
+        if (GameModeStatus.GameMode == GameModeEnum.MULTIPLAYER)
         {
-            isPoppedUp = true;
-            playerTurnPopup.SetActive(true);
-            
-            
-            // Get info from ChallengeManager/Controller
-            if (GameModeStatus.GameMode == GameModeEnum.MULTIPLAYER)
-            {
-                turnText.text = controller.GetPlayerNameForTurn() + "'s Turn!";
-            }
-            else
-            {
-                turnText.text = turnText.text == YOURTURNTEXT ?
-                    OTHERPLAYERTEXT : YOURTURNTEXT;
-            }
-            
-            Debug.Log("turnText value: " + turnText.text);
-            StartCoroutine(PopUp());
+            turnText.text = controller.GetPlayerNameForTurn() + "'s Turn!";
         }
-
+        else
+        {
+            turnText.text = turnText.text == YOURTURNTEXT ?
+                OTHERPLAYERTEXT : YOURTURNTEXT;
+        }
+            
+        Debug.Log("turnText value: " + turnText.text);
+        StartCoroutine(PopUp());
     }
 
-    //void PopUp()
     IEnumerator PopUp()
-    {
-        //playerTurnPopup.SetActive(true);            
+    {        
         float time = 0;
 
         while (time < popupTime)
