1c928872a7e34d9c3ff5d960531cf4786b62f251
bhudgins <35714322+bhudgins@users.noreply.github.com>
2019-03-22 11:02:41 -0500

    WOrked on fonts and main menu

---
+++ Quoridor/Assets/Scripts/UIScripts/ButtonFont.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonFont : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponentInParent<Text>();
+       // Debug.Log(text.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        text.fontStyle = FontStyle.Bold;
+       // Debug.Log("mouseover");
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        text.fontStyle = FontStyle.Normal;
+       // Debug.Log("mouseExit");
+    }
+
+    //public void OnPointerClick(PointerEventData data)
+    //{
+    //    text.fontStyle = FontStyle.Normal;
+    //}
+
+}
