86e4ba6e80cbce1997a427999890dfd07808d67b
softdev <softdev@SI-203-04.harding.edu>
2019-03-27 16:24:33 -0500

    Add tutorial video script

---
+++ Quoridor/Assets/Scripts/UIScripts/TutorialVideo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TutorialVideo : MonoBehaviour
+{
+    private VideoPlayer tutorialVideoPlayer;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialVideoPlayer = GameObject.Find("TutorialVideoPlayer").GetComponent<VideoPlayer>();
+        //tutorialVideoPlayer.clip = Resources.Load<VideoClip>("SampleVideo_640x360_1mb");
+        //tutorialVideoPlayer.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
