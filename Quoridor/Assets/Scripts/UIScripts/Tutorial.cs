61dac0818fcc2720b6f145ef03ddbc1749e5a906
bhudgins <35714322+bhudgins@users.noreply.github.com>
2019-03-30 11:29:31 -0500

    added tutorial text

--- Quoridor/Assets/Scripts/UIScripts/Tutorial.cs
+++ Quoridor/Assets/Scripts/UIScripts/Tutorial.cs
@@ -25,6 +25,8 @@ public class Tutorial : MonoBehaviour
         prevButton = GameObject.Find("PreviousSlideButton");
         nextButton = GameObject.Find("NextSlideButton");
         instructTextBox = GameObject.Find("TutorialText").GetComponent<Text>();
+        instructList = createInstructList();
+        instructTextBox.text = instructList[0];
     }
 
     // Update is called once per frame
@@ -62,6 +64,7 @@ public class Tutorial : MonoBehaviour
         if (index > -1)
         {
             tutorialVideoPlayer.clip = tutorialClips[index];
+            instructTextBox.text = instructList[index];
         }
         else
         {
@@ -76,6 +79,8 @@ public class Tutorial : MonoBehaviour
         if (index < tutorialClips.Length)
         {
             tutorialVideoPlayer.clip = tutorialClips[index];
+
+            instructTextBox.text = instructList[index];
         }
         else
         {
@@ -83,8 +88,16 @@ public class Tutorial : MonoBehaviour
         }
     }
 
-    private void createInstructList()
+    private string[] createInstructList()
     {
+        string[] temp = new string[5];
 
+        temp[0] = "To place a wall, hover over the spot you want to place it and click.";
+        temp[1] = "To move your mouse, click one of the green squares - these show what moves you are allowed to make.";
+        temp[2] = "If your opponent is directly in front of you, and there isn't a wall behind them, you can jump over them.";
+        temp[3] = "If your opponent is directly in front of you, and there is a wall behind them, you can move diagonally to a space on either side of them";
+        temp[4] = "When placing walls, you can place them anywhere, UNLESS placing the wall blocks your's or your opponent's path to the other side.";
+
+        return temp;
     }
 }
