f42f44e82fedc400be290ca93c3c8cc8a86931cb
cspn <cspann1@harding.edu>
2019-04-02 17:07:17 -0500

    Align menus and remove reference to player prefs in audio controller

--- Quoridor/Assets/Scripts/UIScripts/AudioControllerMainMenu.cs
+++ Quoridor/Assets/Scripts/UIScripts/AudioControllerMainMenu.cs
@@ -10,9 +10,9 @@ public class AudioControllerMainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mixer.SetFloat("MasterVolume", Mathf.Log10(PlayerPrefs.GetFloat("MasterVolume", 1.0f)) * 50);
-        mixer.SetFloat("BackgroundVolume", Mathf.Log10(PlayerPrefs.GetFloat("BackgroundVolume", 1.0f)) * 50);
-        mixer.SetFloat("EffectsVolume", Mathf.Log10(PlayerPrefs.GetFloat("EffectsVolume", 1.0f)) * 50);
+        mixer.SetFloat("MasterVolume", Mathf.Log10(GameSession.MasterVolumePref) * 50);
+        mixer.SetFloat("BackgroundVolume", Mathf.Log10(GameSession.MasterVolumePref) * 50);
+        mixer.SetFloat("EffectsVolume", Mathf.Log10(GameSession.MasterVolumePref) * 50);
     }
     
 }
