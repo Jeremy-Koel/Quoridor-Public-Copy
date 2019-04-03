0791c3524261ed99cc801882dd844a5cd7d29b7a
bhudgins <35714322+bhudgins@users.noreply.github.com>
2019-04-03 12:14:34 -0500

    added some tutorial slides

--- Quoridor/Assets/Scripts/UIScripts/Tutorial.cs
+++ Quoridor/Assets/Scripts/UIScripts/Tutorial.cs
@@ -8,20 +8,23 @@ public class Tutorial : MonoBehaviour
 {
     private VideoPlayer tutorialVideoPlayer;
     private VideoClip[] tutorialClips;
-    private int index = 0;
+    private int index = -1;
     private GameObject prevButton;
     private GameObject nextButton;
     private string[] instructList;
     private Text instructTextBox;
-
+    private GameObject tutorialImage;
+    private GameObject logoImage;
+    private int numberOfTutorialSlides = 7;
     // Start is called before the first frame update
     void Start()
     {
         tutorialVideoPlayer = GameObject.Find("TutorialVideoPlayer").GetComponent<VideoPlayer>();
         tutorialClips = Resources.LoadAll<VideoClip>("tutorialVideos");
-
-        tutorialVideoPlayer.clip = tutorialClips[index];
-
+        logoImage = GameObject.Find("LogoTutorialImage");
+        tutorialImage = GameObject.Find("TutorialRawImage");
+       // tutorialVideoPlayer.clip = tutorialClips[index];
+        tutorialImage.SetActive(false);
         prevButton = GameObject.Find("PreviousSlideButton");
         nextButton = GameObject.Find("NextSlideButton");
         instructTextBox = GameObject.Find("TutorialText").GetComponent<Text>();
@@ -32,25 +35,31 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(index == tutorialClips.Length - 1 && index > 0)
+        if(index == numberOfTutorialSlides - 2 && index > -1)
         {
             nextButton.SetActive(false);
             prevButton.SetActive(true);
+            //logoImage.SetActive(false);
+            //tutorialImage.SetActive(true);
         }
 
-        if (index == 0 && index < tutorialClips.Length - 1)
+        if (index == -1 && index < numberOfTutorialSlides - 2)
         {
             prevButton.SetActive(false);
             nextButton.SetActive(true);
+            //tutorialImage.SetActive(false);
+            //logoImage.SetActive(true);
+
         }
 
-        if (index > 0 && index < tutorialClips.Length - 1)
+        if (index > -1 && index < numberOfTutorialSlides - 2)
         {
+
             nextButton.SetActive(true);
             prevButton.SetActive(true);
         }
 
-        if(tutorialClips.Length == 1)
+        if(numberOfTutorialSlides == 1)
         {
             nextButton.SetActive(false);
             prevButton.SetActive(false);
@@ -61,14 +70,26 @@ public class Tutorial : MonoBehaviour
     {
         index -= 1;
 
-        if (index > -1)
+        if (index > -2)
         {
-            tutorialVideoPlayer.clip = tutorialClips[index];
-            instructTextBox.text = instructList[index];
+            if (index == -1)
+            {
+                logoImage.SetActive(true);
+                tutorialImage.SetActive(false);
+                instructTextBox.text = instructList[index + 1];
+            }
+            else if (index > -1)
+            {
+                logoImage.SetActive(false);
+                tutorialImage.SetActive(true);
+
+                tutorialVideoPlayer.clip = tutorialClips[index];
+                instructTextBox.text = instructList[index + 1];
+            }
         }
         else
         {
-            index = 0;
+            index = -1;
         }
     }
 
@@ -76,28 +97,31 @@ public class Tutorial : MonoBehaviour
     {
         index += 1;
 
-        if (index < tutorialClips.Length)
+        if (index < numberOfTutorialSlides)
         {
+            logoImage.SetActive(false);
+            tutorialImage.SetActive(true);
             tutorialVideoPlayer.clip = tutorialClips[index];
 
-            instructTextBox.text = instructList[index];
+            instructTextBox.text = instructList[index + 1];
         }
         else
         {
-            index = tutorialClips.Length - 1;
+            index = numberOfTutorialSlides - 1;
         }
     }
 
     private string[] createInstructList()
     {
-        string[] temp = new string[5];
+        string[] temp = new string[7];
 
-        
-        temp[0] = "To move your mouse, click one of the green squares - these show what moves you are allowed to make.";
-        temp[1] = "To place a wall, hover over the spot you want to place it and click.";
-        temp[2] = "If your opponent is directly in front of you, and there isn't a wall behind them, you can jump over them.";
-        temp[3] = "If your opponent is directly in front of you, and there is a wall behind them, you can move diagonally to a space on either side of them.";
-        temp[4] = "When placing walls, you can place them anywhere, UNLESS placing the wall blocks you or your opponent from reaching their goal.";
+        temp[0] = "Welcome to the Great Gouda Gambit! Our scientists are training our labrats to play Quoridor. Think you can help?";
+        temp[1] = "To move your mouse, click one of the green squares.";
+        temp[2] = "To place a wall, hover over the spot you want to place it and click.";
+        temp[3] = "If your opponent is directly in front of you, and there isn't a wall behind them, you can jump over them.";
+        temp[4] = "If your opponent is directly in front of you, and there is a wall behind them, you can move diagonally to a space on either side of them.";
+        temp[5] = "When placing walls, you can place them anywhere, UNLESS placing the wall blocks you or your opponent from reaching their goal.";
+        temp[6] = "Walls also cannot overlap.";
 
         return temp;
     }
