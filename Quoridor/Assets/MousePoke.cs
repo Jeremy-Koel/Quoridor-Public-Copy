111b8101493616ec8a403068045eb2a9af47aceb
cspn <cspann1@harding.edu>
2019-04-09 15:57:28 -0500

    Make mouse on main menu squeak when clicked

---
+++ Quoridor/Assets/MousePoke.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePoke : MonoBehaviour
+{
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GameObject.Find("MouseSqueakEffectPlayer").GetComponent<AudioSource>();
+    }
+
+    public void OnMousePoked()
+    {
+        source.Play();
+    }
+}
