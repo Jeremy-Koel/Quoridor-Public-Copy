53a585d6e1f408ef7ac34a8623023a74d917f26a
bhudgins <35714322+bhudgins@users.noreply.github.com>
2019-04-13 11:52:54 -0500

    added exit sign

---
+++ Quoridor/Assets/Scripts/UIScripts/ExitSign.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitSign : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        transform.localScale = new Vector3(transform.localScale.x + .1f, transform.localScale.y + .1f, transform.localScale.z);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        transform.localScale = new Vector3(transform.localScale.x - .1f, transform.localScale.y - .1f, transform.localScale.z);
+    }
+
+}
