3aeaeb19f5dc986b871ddaa70f220b9332e45f97
cspn <cspann1@harding.edu>
2019-04-14 23:49:02 -0500

    Update lobby chat

---
+++ Quoridor/Assets/Scripts/ButtonScripts/ChatHover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChatHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private FriendsChatPanel chatPanel;
+
+    private void Awake()
+    {
+        chatPanel = GameObject.Find("FriendsChatPanel").GetComponent<FriendsChatPanel>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        chatPanel.HoverUp();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        chatPanel.HoverDown();
+    }
+}
--- Quoridor/Assets/Scripts/UIScripts/FriendsChatPanel.cs
+++ Quoridor/Assets/Scripts/UIScripts/FriendsChatPanel.cs
@@ -2,103 +2,180 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class FriendsChatPanel : MonoBehaviour
 {
-    Button friendsChatPanelButton;
+    Button openChatButton;
     private GameObject scientistArmOne;
     private GameObject friendsChatPanel;
+    private GameObject openChatGameObject;
+    private GameObject minimizeChat;
+    private GameObject friendsPanel;
+    private GameObject chatSelectionPanel;
+    private GameObject chatWindowPanel;
     private RectTransform friendsChatPanelRectTransform;
     private MoveArms moveArmsOne;
     bool chatActive = false;
     bool movePDA = false;
     bool moveHand = false;
+    bool pointerIn = false;
+    bool pointerOut = false;
     public float speed = 2000.0f;
+    private float step;
     public Vector2 target;
     public Vector2 activeAnchorForChatPosition;
     public Vector2 inactiveAnchorForChatPosition;
     public Vector2 activeAnchorForArmPosition;
     public Vector2 originalAnchorForArmPosition;
 
+    private Vector3 activePosition;
+    private Vector3 inactivePosition;
+    private Vector3 hoverOverPosition;
+
     private void Awake()
     {
-        friendsChatPanelButton = GameObject.Find("FriendsAndChatButton").GetComponent<Button>();
-        scientistArmOne = GameObject.Find("ScientistArmOne");
-        moveArmsOne = scientistArmOne.GetComponent<MoveArms>();
-        //activeAnchorForChatPosition = GameObject.Find("ActiveAnchorForChatPosition").GetComponent<RectTransform>().position;
-        //inactiveAnchorForChatPosition = GameObject.Find("InactiveAnchorForChatPosition").GetComponent<RectTransform>().position;
+        openChatGameObject = GameObject.Find("OpenChatButton");
+        openChatButton = openChatGameObject.GetComponent<Button>();
+        //scientistArmOne = GameObject.Find("ScientistArmOne");
+        //moveArmsOne = scientistArmOne.GetComponent<MoveArms>();
         friendsChatPanel = GameObject.Find("FriendsChatPanel");
         friendsChatPanelRectTransform = friendsChatPanel.GetComponent<RectTransform>();
-
-        //friendsChatPanelButton.onClick.AddListener(OnChatClick);
+        minimizeChat = GameObject.Find("MinimizeChatButton");
+        friendsPanel = GameObject.Find("FriendsPanel");
+        chatSelectionPanel = GameObject.Find("ChatSelectionPanel");
+        chatWindowPanel = GameObject.Find("ChatWindowPanel");
+        activePosition = new Vector3(814f, 35f, 0f);
+        inactivePosition = new Vector3(814f, -499f, 0f);
+        hoverOverPosition = new Vector3(814f, -450f, 0f);
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToggleChatElements();
+        step = FastAnimations.CalculateSpeed(speed);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (movePDA)
+        //{
+        //    if (moveHand)
+        //    {
+        //        if ((scientistArmOne.GetComponent<RectTransform>().localPosition.x == scientistArmOne.GetComponent<MoveArms>().chatTarget.x &&
+        //                scientistArmOne.GetComponent<RectTransform>().localPosition.y == scientistArmOne.GetComponent<MoveArms>().chatTarget.y))
+        //        {
+        //            moveHand = false;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //float step = speed * Time.deltaTime; // calculate distance to move
+        //        float step = FastAnimations.CalculateSpeed(speed);
+        //        Vector2 transformPosition = new Vector2(friendsChatPanelRectTransform.localPosition.x, friendsChatPanelRectTransform.localPosition.y);
+        //        friendsChatPanelRectTransform.localPosition = Vector2.MoveTowards(transformPosition, target, step);
+        //        SetTarget(friendsChatPanelRectTransform.localPosition);
+
+        //        if (friendsChatPanelRectTransform.localPosition.x == target.x &&
+        //                friendsChatPanelRectTransform.localPosition.y == target.y)
+        //        {
+        //            movePDA = false;
+        //            SetTarget(inactiveAnchorForChatPosition);
+        //        }
+        //    }
+        //}
+
         if (movePDA)
         {
-            if (moveHand)
-            {
-                if ((scientistArmOne.GetComponent<RectTransform>().localPosition.x == scientistArmOne.GetComponent<MoveArms>().chatTarget.x &&
-                        scientistArmOne.GetComponent<RectTransform>().localPosition.y == scientistArmOne.GetComponent<MoveArms>().chatTarget.y))
-                {
-                    moveHand = false;
-                }
-            }
-            else
-            {
-                //float step = speed * Time.deltaTime; // calculate distance to move
-                float step = FastAnimations.CalculateSpeed(speed);
-                Vector2 transformPosition = new Vector2(friendsChatPanelRectTransform.localPosition.x, friendsChatPanelRectTransform.localPosition.y);
-                friendsChatPanelRectTransform.localPosition = Vector2.MoveTowards(transformPosition, target, step);
-                SetTarget(friendsChatPanelRectTransform.localPosition);
 
-                if (friendsChatPanelRectTransform.localPosition.x == target.x &&
-                        friendsChatPanelRectTransform.localPosition.y == target.y)
+            if (chatActive) // Showing PDA
+            {
+                friendsChatPanelRectTransform.localPosition = Vector3.MoveTowards(friendsChatPanelRectTransform.localPosition, activePosition, step);
+                
+                // Did we reach our destination?
+                if (friendsChatPanelRectTransform.localPosition.Equals(activePosition))
                 {
                     movePDA = false;
-                    SetTarget(inactiveAnchorForChatPosition);
                 }
+            }
+            else // Hiding PDA 
+            {
+                friendsChatPanelRectTransform.localPosition = Vector3.MoveTowards(friendsChatPanelRectTransform.localPosition, inactivePosition, step);
+
+                // Did we reach our destination?
+                if (friendsChatPanelRectTransform.localPosition.Equals(inactivePosition))
+                {
+                    movePDA = false;
+                }
+            }
+        }
+        else if (pointerIn)
+        {
+            friendsChatPanelRectTransform.localPosition = Vector3.MoveTowards(friendsChatPanelRectTransform.localPosition, hoverOverPosition, step);
+
+            // Did we reach our destination?
+            if (friendsChatPanelRectTransform.localPosition.Equals(hoverOverPosition))
+            {
+                pointerIn = false;
+            }
+        }
+        else if (pointerOut)
+        {
+            friendsChatPanelRectTransform.localPosition = Vector3.MoveTowards(friendsChatPanelRectTransform.localPosition, inactivePosition, step);
+
+            // Did we reach our destination?
+            if (friendsChatPanelRectTransform.localPosition.Equals(inactivePosition))
+            {
+                pointerOut = false;
             }
         }
     }
 
     public void OnChatClick()
     {
+        // Force chat panel to redraw itself so new chats are added 
         LayoutRebuilder.ForceRebuildLayoutImmediate(friendsChatPanelRectTransform);
         LayoutRebuilder.MarkLayoutForRebuild(friendsChatPanelRectTransform);
         friendsChatPanelRectTransform.gameObject.SetActive(false);
         friendsChatPanelRectTransform.gameObject.SetActive(true);
-        if (chatActive)
-        {
-            moveHand = true;
-            SetTarget(activeAnchorForChatPosition);
-            target = inactiveAnchorForChatPosition;
-            chatActive = false;
-        }
-        else
-        {
-            target = activeAnchorForChatPosition;
-            chatActive = true;
-            SetTarget(target);
-        }
-        
-        moveArmsOne.moveArm = true;
+
         movePDA = true;
+        chatActive = !chatActive;
+
+        // If the chat is active, then we don't want the open button to be interactable 
+        openChatGameObject.SetActive(!chatActive);
+
+        ToggleChatElements();
+    }
+
+    private void ToggleChatElements()
+    {
+        // For these elements, we want them to show if the chat is active (open and showing)
+        minimizeChat.SetActive(chatActive);
+        friendsPanel.SetActive(chatActive);
+        chatSelectionPanel.SetActive(chatActive);
+        chatWindowPanel.SetActive(chatActive);
+    }
+
+    public void HoverUp()
+    {
+        pointerIn = true;
+    }
+
+    public void HoverDown()
+    {
+        pointerOut = true;
     }
 
     public void SetTarget(Vector2 pos)
     {
-        pos.x = pos.x - 120;
-        pos.y = pos.y - 1508;
+        //pos.x = pos.x - 120;
+        //pos.y = pos.y - 1508;
 
-        moveArmsOne.SetTargetChat(pos);
+        //moveArmsOne.SetTargetChat(pos);
     }
+
 }
